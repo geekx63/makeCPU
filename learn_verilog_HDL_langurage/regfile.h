b925b59731a5265681e9c44e7dd4360d4074221a
geekxing <geekx63@gmail.com>
2015-01-18 21:39:20 +0800

    learn verilog

---
+++ learn_verilog_HDL_langurage/regfile.h
@@ -0,0 +1,21 @@
+`ifndef __REGFILE_HEADER__		//包含文件防范
+	`define __REGFILE_HEADER__
+
+	/****信号电平****/
+	`define	HIGH		1'b0	//高电平
+	`define	LOW		1'b1	//低电平
+
+
+	/****逻辑值****/
+	`define	ENABLE_		1'b0	//有效{}
+	`define	DISABLE_	1'b1	//无效
+
+	/****数据****/
+	`define	DATA_W		32	//数据宽度
+	`define	DataBus		31:0	//数据总线
+	`define	DATA_D		32	//数据深度
+
+	/****地址****/
+	`define	ADDR_W		5	//地址宽度
+	`define	AddrBus		4:0	//地址总线
+`endif
